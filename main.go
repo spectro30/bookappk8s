619fae58fb4f07094d1d10603c9228ab207f1605
Alif Biswas <alif@appscode.com>
2020-08-18 15:33:42 +0600

    inital commit

--- main.go
+++ main.go
@@ -1,31 +1,109 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
 	"path/filepath"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
 	"k8s.io/client-go/tools/clientcmd"
+
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatal(err)
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
-	kc, err := kubernetes.NewForConfig(config)
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err)
 	}
-	pods, err := kc.CoreV1().Pods("kube-system").List(v1.ListOptions{})
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name, "<===>", pod.Namespace)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "bookapp-deployment",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "bookapp",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "bookapp",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "bookapp",
+							Image: "spectro30/bookapp:latest",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: 8888,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
+
+	fmt.Println("Creating deployment")
+
+	dep, err := clientset.AppsV1().Deployments(corev1.NamespaceDefault).Create(deployment)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", dep.GetObjectMeta().GetName())
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "bookapp-service",
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"app": "bookapp",
+			},
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					Port: 80,
+					TargetPort: intstr.IntOrString{
+						IntVal: 8888,
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Println("Creating service")
+	serv, err := clientset.CoreV1().Services("default").Create(service)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created Service %q.\n", serv.GetObjectMeta().GetName())
+
 }
+
+func int32Ptr(i int32) *int32 { return &i }
